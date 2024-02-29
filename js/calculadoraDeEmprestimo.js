c9d09e87c62982272479dc33e0f612ec2256d8e1
= <renatomdavid811@hotmail.com>
2024-02-29 15:42:56 -0300

    realizando teste

--- js/calculadoraDeEmprestimo.js
+++ js/calculadoraDeEmprestimo.js
@@ -86,6 +86,70 @@ var url = "getlenders.php"+"?amt="
  graph.width = graph.width;
  if(arguments.length == 0 || !graph.getContext)return;
  var g = graph.getContext("2d");
- var width = graph.width, height = graph.height;
+ var width = graph.width, height = graph.height;//obtem o tamanho da tela e do desenho
+
+ function paymentToX(n){
+    return n * width/payments;
+ }
+ function amountToY(a){
+    return height-(a* height/(monthly*payments*1.05))
+ }
+g.moveTo(paymentToX(0),amountToY(0));
+g.lineTo(paymentToX(payments),
+amountToY(monthly*payments));
+g.lineTo(paymentToX(payments), amountToY(0));
+g.closePath();
+g.fillStlyle = "#f88";
+g.fill();
+g.font = "bold 12px sans-serif";
+g.fillText("total interest payments", 20, 20);
+
+var equity = 0;
+g.beginPath();
+g.moveTo(paymentToX(0), amountToY(0));
+for(var p = 1; p <= payments; p++){
+    var thisMonthInterest = (principal-equity)*interest;
+    g.lineTo(paymentToX(p),amountToY(equity));
+}   
+g.lineTo(paymentToX(payments), amountToY(0));
+g.closePath();
+g.fillStlyle= "green";
+g.fill();
+g.fillText("total equity", 20,35);
+
+var bal = principal;
+g.beginPath();
+g.moveTo(paymentToX(p),amountToY(bal));
+for(var p = 1; p<=payments; p++){
+    var thisMonthInterest = bal*interest;
+    bal -=(monthly - thisMonthInterest);
+    g.lineTo(paymentToX(p),amountToY(bal));
+}
+g.lineWidth = 3;
+g.stroke();
+g.fillStlyle="black";
+g.fillText("Loan Balance", 20,50);
+
+g.textAlign = "center";
+
+var y = amountToY(0);
+for(var year=1;year*12 <= payments; year++){
+    var x = paymentToX(year*12);
+    g.fillRect(x-0.5,y-3,1,3);
+    if(year==1) g.fillText("Year", x, y-5);
+    
+    if(year%5 == 0 && year*12 !== payments)
+    g.fillText(String(year),x,y-5);
+}
+g.textAlign = "rigth";
+g.textBaseline = "middle";
+var ticks = [ monthly*payments, principal];
+var rightEdge = paymentToX(payments);
+for(var i = 0; i< ticks.length; i++){
+    var y = amountToY(ticks[i]);
+    g.fillRect(rigthEdge-3,y-0.5, 3,1);
+    g.fillText(String(ticks[i].toFixed(0)),
+    rightEdge-5,y);
+}
 
 }
